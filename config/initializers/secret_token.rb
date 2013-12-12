4c38ac99fab0661f2b4a5913bb8de025341ea92f
Vy <vy@substantial.com>
2013-12-11 20:49:45 -0800

    initial rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gearstantial::Application.config.secret_key_base = 'ad7c1245d92b7b99dbf4e04a29dce3c2270afb05b9a0553a7cd1b0fec852af971fa8e4c2989070820fcdb43cef71f2f227f2bb1397c58630d807428f478b1877'
