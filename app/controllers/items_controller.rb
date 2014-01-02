d75915960d8966ab788bab1e2f66716293546fa7
Vy <vy@substantial.com>
2014-01-02 15:11:29 -0800

    added new arguments to item, created show action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,18 @@
 class ItemsController < ApplicationController
 
   def new 
-
   end
 
   def create
-    item = Item.new(name: params[:items][:name], serial: params[:items][:serial], category: params[:items][:category], year: params[:items][:year])
+    item = Item.new(name: params[:items][:name], serial: params[:items][:serial], category: params[:items][:category], year: params[:items][:year], purchase_date: params[:items][:purchase_date], purchase_price: params[:items][:purchase_price])
     item.save
 
     flash[:notice] = "Item was created with id##{item.id}"
     redirect_to '/items', flash: flash
+  end
+
+  def show
+    @item = Item.find(params[:id])   
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Gearstantial::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'items#index'
 
   # Example of regular route:
   get 'items/new' => 'items#new'
   get 'items/edit' => 'items#edit'
+  get 'items/:id' => 'items#show' #should I just put item :id here instead? items/:item_id/items/:id
   get 'items' => 'items#index'
   post 'items' => 'items#create' 
+
+  # You can have the root of your site routed with "root"
+  root 'items#index'
 end
