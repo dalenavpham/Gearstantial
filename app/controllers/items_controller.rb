c223700c6ca9b96f46fa4874d3324730f4731bd1
Vy <vy@substantial.com>
2014-01-15 18:45:33 -0800

    added destroy page & styled index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,7 @@ class ItemsController < ApplicationController
     item = Item.new(name: params[:items][:name], serial: params[:items][:serial], category: params[:items][:category], year: params[:items][:year], purchase_date: params[:items][:purchase_date], purchase_price: params[:items][:purchase_price])
     item.save
 
-    flash[:notice] = "Item was created with id# {item.id}"
+    flash[:notice] = "Item was created with id # #{item.id}"
     redirect_to '/items', flash: flash
   end
 
@@ -19,11 +19,26 @@ class ItemsController < ApplicationController
     #similar to show, but you are resubmitting the data and replacing its contents with the new content
     # common with an if statement (sucess or not?)
     @item = Item.find(params[:id])  
-    
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(name: params[:items][:name], serial: params[:items][:serial], category: params[:items][:category], year: params[:items][:year], purchase_date: params[:items][:purchase_date], purchase_price: params[:items][:purchase_price])
+    item.save
+
+    flash[:notice] = "Item was updated with id # #{item.id}"
+    redirect_to '/items', flash: flash
   end
 
   def index
     @items = Item.all
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    flash[:notice] = "Item name #{item.name} was deleted"
+    redirect_to '/items', flash: flash
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Gearstantial::Application.routes.draw do
 
 
   # Example of regular route:
-  resources :items
+  get 'items/new' => 'items#new', :as => 'new_item'
+  get 'items/:id/edit' => 'items#edit'
+  get 'items/:id' => 'items#show' #should I just put item :id here instead? items/:item_id/items/:id
+  get 'items' => 'items#index'
+  put 'items/:id' => 'items#update'
+  delete 'items/:id' => 'items#destroy'
+  post 'items' => 'items#create', :as => 'create_item' 
 
   # You can have the root of your site routed with "root"
   root 'items#index'
